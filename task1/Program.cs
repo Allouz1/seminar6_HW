54f67dfba116af8513a77fe5f2a36d120142c920
Max <makism.mush21@gmail.com>
2022-10-20 11:46:56 +0300

    HW

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите несколько чисел Через запятую: ");
+string strNumbers = Console.ReadLine();
+
+int countNumbers = 1;
+
+for (int i = 0; i < strNumbers.Length; i++)
+{
+    if (strNumbers[i] == ',')
+        countNumbers++;
+}
+
+int[] numbers = new int [countNumbers];
+int indexNumber = 0;
+string substring = string.Empty;
+for (int i = 0; i < strNumbers.Length; i++)
+{
+    if( strNumbers[i] != ',')
+    {
+        substring += strNumbers[i];
+    }
+    else
+    {
+        numbers[indexNumber] = Convert.ToInt32(substring);
+        indexNumber++;
+        substring = string.Empty;
+    }
+}
+
+numbers[indexNumber] = Convert.ToInt32(substring);
+
---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿double b1 = ReadInt("Задайте значение b1: ");
+double k1 = ReadInt("Задайте значение k1: ");
+double b2 = ReadInt("Задайте значение b2: ");
+double k2 = ReadInt("Задайте значение k2: ");
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = x * k1 + b1;
+    Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+}
+
+
+
+
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());       
+}
